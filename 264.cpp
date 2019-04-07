0b1fe0043178a055545f180e40cd5ea2de409021
Jinxin Wang <jianhuqiuxia@gmail.com>
2019-04-06 23:06:26 -0700

    dynamic programming problem

---
+++ 264.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ull = uint64_t;
+using ll = int64_t;
+using ld = long double;
+
+const int n_multiplier = 3;
+const int multipliers[] = {2, 3, 5};
+
+int nthUglyNumber(int n)
+{
+	set<int> s;
+	priority_queue<int, greater<int> > q;
+	s.insert(1);
+	q.push(1);
+	int id = 0, ans = 0;
+	while (!q.empty())
+	{
+		ans = q.top();
+		++id;
+		q.pop();
+		if (id == n)
+			break;
+		for (int i = 0; i != n_multiplier; ++i)
+		{
+			int next = ans * multipliers[i];
+			if (!s.count(next))
+			{
+				s.insert(next);
+				q.push(next);
+			}
+		}
+	}
+	return ans;
+}
+
