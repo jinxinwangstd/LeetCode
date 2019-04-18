5230e921e15900e6a05ab7ff639ca1d42c828e15
Jinxin Wang <jianhuqiuxia@gmail.com>
2019-04-18 09:15:31 -0700

    common string prefix

--- 14.cpp
+++ 14.cpp
@@ -8,6 +8,7 @@ using ld = long double;
 class Solution
 {
 public:
+	// Synchronously linear scan each string and select the common first n characters
 	string longestCommonPrefix(vector<string>& strs)
 	{
 		int num = (int) strs.size();
@@ -18,14 +19,14 @@ public:
 		int i = 0;
 		while (true)
 		{
-			if (i == (int) strs[0].size())
+			if (i == (int) strs[0].size())		// reach the end of the first string
 				return ret;
-			c = strs[0][i];
+			c = strs[0][i];						// the character we need to scan at this iteration
 			for (int j = 1; j != num; ++j)
 			{
-				if (i == (int) strs[j].size())
+				if (i == (int) strs[j].size())	// reach the end of one string
 					return ret;
-				if (strs[j][i] != c)
+				if (strs[j][i] != c)			// not common in all strings, then return all characters before it
 					return ret;
 			}
 			ret.push_back(c);
