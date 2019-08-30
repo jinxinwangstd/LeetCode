979e6ad4cc99c660ea0610e6269c07264c3b4631
Jinxin Wang <jianhuqiuxia@gmail.com>
2019-08-29 17:28:43 -0700

    08/29/2019

--- 546.cpp
+++ 546.cpp
@@ -11,7 +11,27 @@ typedef vector<ii> vii;
 
 class Solution {
 public:
+    int calculateMaxPoints(vector< vector<vi> >& table, vi& boxes, int left, int right, int dups) {
+        if (left > right)
+            return 0;
+        if (table[left][right][dups] != 0)
+            return table[left][right][dups];
+        // calculate the tailing repeated elements
+        while (right > left && boxes[right] == boxes[right - 1]) {
+            right--;
+            dups++;
+        }
+        table[left][right][dups] = calculateMaxPoints(table, boxes, left, right, dups);
+        for (int i = left; i < right; ++i) {
+            if (boxes[i] == boxes[right])
+                table[left][right][dups] = max(table[left][right][dups], 
+                        calculateMaxPoints(table, boxes, left, i, dups + 1) + calculateMaxPoints(table, boxes, i + 1, right - 1, 0));
+        }
+        return table[left][right][dups];
+    }
     int removeBoxes(vector<int>& boxes) {
-        
+        int n = (int)boxes.size();
+        vector< vector<vi> > table(n, vector<vi>(n, vi(n, 0)));
+        return calculateMaxPoints(table, boxes, 0, n - 1, 0);
     }
 };
