7e59831682e383fd2e97a94970ddfb51a2b7b2c4
Jinxin Wang <jianhuqiuxia@gmail.com>
2019-09-06 09:23:08 -0700

    09/06/2019

--- 435.cpp
+++ 435.cpp
@@ -8,6 +8,23 @@ using ld = long double;
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        
+        sort(intervals.begin(), intervals.end());
+        int count = 0;
+        vector<vector<int>>::iterator iter = intervals.begin();
+        while (iter != intervals.end()) {
+            vector<vector<int>>::iterator next_iter;
+            if ((next_iter = next(iter, 1)) == intervals.end())
+                break;
+            if ((*next_iter)[0] < (*iter)[1]) {
+                if ((*iter)[1] > (*next_iter)[1])
+                    iter = intervals.erase(iter);
+                else
+                    intervals.erase(next_iter);
+                count++;
+            }
+            else
+                iter++;
+        }
+        return count;
     }
 };
--- 630.cpp
+++ 630.cpp
@@ -7,7 +7,28 @@ using ld = long double;
 
 class Solution {
 public:
+    static bool compare(vector<int>& p, vector<int>& q) {
+        return p[1] < q[1];
+    }
     int scheduleCourse(vector<vector<int>>& courses) {
-
+        sort(courses.begin(), courses.end(), compare);
+        int n = courses.size();
+        priority_queue<int> pq;
+        int time = 0;
+        for (int i = 0; i < n; i++) {
+            if (time + courses[i][0] <= courses[i][1]) {
+                time += courses[i][0];
+                pq.push(courses[i][0]);
+            }
+            else if (!pq.empty()) {
+                int max_time = pq.top();
+                if (max_time > courses[i][0]) {
+                    pq.pop();
+                    time = time - max_time + courses[i][0];
+                    pq.push(courses[i][0]);
+                }
+            }
+        }
+        return pq.size();
     }
 };
