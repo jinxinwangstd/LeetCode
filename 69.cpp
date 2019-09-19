f4630532ad7b414e9772a9bfdfc4bc2877614868
Jinxin Wang <jianhuqiuxia@gmail.com>
2019-09-19 13:48:50 -0700

    09/19/2019

--- 69.cpp
+++ 69.cpp
@@ -9,31 +9,26 @@ typedef vector<int> vi;
 typedef pair<int, int> ii;
 typedef vector<ii> vii;
 
-class Solution
-{
+class Solution {
 public:
-	// Calculate the square root based on the divide and conquer algorithms
-	int mySqrt(int x)
-	{
-		int hi = x, lo = 0;
-		int mid = 0;
-		while (hi >= lo)
-		{
-			mid = (hi + lo) / 2;
-			// watch out overflow risk
-			if ((ll) mid * mid > x)
-			{
-				hi = mid - 1;
-			}
-			else if ((ll) mid * mid < x)
-			{
-				lo = mid + 1;
-			}
-			else
-				return mid;
-		}
-		if (mid * mid > x)		// we only return the truncated result so the square of the result must be <= x
-			--mid;
-		return mid;
-	}
+    // Calculate the square root based on the divide and conquer algorithms
+    int mySqrt(int x) {
+        int hi = x, lo = 0;
+        int mid = 0;
+        while (hi >= lo) {
+            mid = (hi + lo) / 2;
+            // watch out overflow risk
+            if ((ll) mid * mid > x) {
+                hi = mid - 1;
+            }
+            else if ((ll) mid * mid < x) {
+                lo = mid + 1;
+            }
+            else
+                return mid;
+        }
+        if (mid * mid > x)// we only return the truncated result so the square of the result must be <= x
+            --mid;
+        return mid;
+    }
 };
