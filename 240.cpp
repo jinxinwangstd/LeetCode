2224d1392be3686aa284e9ca9495f54474ae13fa
Jinxin Wang <jianhuqiuxia@gmail.com>
2019-09-21 11:38:49 -0700

    09/21/2019

--- 240.cpp
+++ 240.cpp
@@ -4,16 +4,21 @@ public:
         if (matrix.empty() || matrix[0].empty())
             return false;
         int m = matrix.size(), n = matrix[0].size();
-        for (int i = 0; i < m; i++) {
+        int i = 0, j = m - 1;
+        while (i < m && target > matrix[i][n - 1])
+            i++;
+        while (j >= 0 && target < matrix[j][0])
+            j--;
+        for (int row = i; row <= j; row++) {
             int l = 0, r = n - 1, mid;
             while (r - l > 1) {
                 mid = l + (r - l) / 2;
-                if (matrix[i][mid] < target)
+                if (matrix[row][mid] < target)
                     l = mid;
                 else
                     r = mid;
             }
-            if (matrix[i][l] == target || matrix[i][r] == target)
+            if (matrix[row][l] == target || matrix[row][r] == target)
                 return true;
         }
         return false;
